2ef6db5d2be3e9437c7e30066e5ca923dc9d2eb0
Joao Betancourth <frvajoao@gmail.com>
2020-06-29 18:58:56 -0500

    creacion de segunda con el feature 2 -> CoroutineScope

--- app/src/main/java/com/home/coroutines_example/MainActivity.kt
+++ app/src/main/java/com/home/coroutines_example/MainActivity.kt
@@ -5,25 +5,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job //operaciones de ui + Job para tener control de la cancelacion de dicha corrutina en el OnDestroy
+
+    lateinit var job: Job
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        job = SupervisorJob()//Utilizamos el SupervisorJob en vez del Job es que con el "Job" básico tiene como problema
+        // es que si tenemos varias corrutinas y una de ella falla, las demás se cancelarán y con el "SupervisorJob" si una falla,
+        // las demás continúan su flujo
+
         submit.setOnClickListener {
-            GlobalScope.launch(Dispatchers.Main) {
-                //Al elegir el .Main, se decide que esa rutina se realizara en el hilo principal
-                //Ahora necesitamos que la siguiente línea donde valida el Login a un servidor, etc.
-                // Este se realice en un segundo hilo, para esto
-                //utilizaremos un **Suspend / withContext**
-                //**Suspend / withContext** = define que la funcion bloqueará el codigo en el punto donde es llamado, ejecuta la operación que tenga que realizar
-                //y cuando vuelva al hilo principal, devolvera el resultado del codigo u operacion esperado en la variable asignada.
+            launch{//Ya no hay que elegir un dispatcher ya que desde el scope estamos manejando el "Dispatchers.Main "
+                //Con esto ya no nos tenemos que preocupar por el ciclo de vida del activity, si muere, así mismo morirá lo que
+                // esté en segundo plano
                 val success = withContext(Dispatchers.IO) {
                     validateLogin(username.text.toString(), password.text.toString())
                 }
@@ -41,4 +44,10 @@ class MainActivity : AppCompatActivity() {
     private fun Context.toast(message: String){
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
+
+    override fun onDestroy() {
+        job.cancel()
+        super.onDestroy()
+    }
+
 }
