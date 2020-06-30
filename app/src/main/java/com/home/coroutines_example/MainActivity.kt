0c31d50cf3cbe36d91b8d0cf47f94792b7cc73a8
Joao Betancourth <frvajoao@gmail.com>
2020-06-29 19:06:47 -0500

    creacion de tercera rama con el feature 2 -> lifecycleScope

--- app/src/main/java/com/home/coroutines_example/MainActivity.kt
+++ app/src/main/java/com/home/coroutines_example/MainActivity.kt
@@ -4,35 +4,25 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity(), CoroutineScope {
-
-    override val coroutineContext: CoroutineContext
-        get() = Dispatchers.Main + job //operaciones de ui + Job para tener control de la cancelacion de dicha corrutina en el OnDestroy
-
-    lateinit var job: Job
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        job = SupervisorJob()//Utilizamos el SupervisorJob en vez del Job es que con el "Job" básico tiene como problema
-        // es que si tenemos varias corrutinas y una de ella falla, las demás se cancelarán y con el "SupervisorJob" si una falla,
-        // las demás continúan su flujo
-
         submit.setOnClickListener {
-            launch{//Ya no hay que elegir un dispatcher ya que desde el scope estamos manejando el "Dispatchers.Main "
-                //Con esto ya no nos tenemos que preocupar por el ciclo de vida del activity, si muere, así mismo morirá lo que
-                // esté en segundo plano
+            lifecycleScope.launch{// Al utilizar lifecycleScope ya no tenemos que implementar niguna interfaz de Scope,
+                // tampoco debemos preocuparnos por llamar al OnDestroy, ni nada de eso ya que el lifeCycle hará lo necesario por debajo.
                 val success = withContext(Dispatchers.IO) {
                     validateLogin(username.text.toString(), password.text.toString())
                 }
                 toast(if(success) "Success" else "Failure")
             }
-
         }
     }
 
@@ -43,11 +33,6 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
 
     private fun Context.toast(message: String){
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-    }
-
-    override fun onDestroy() {
-        job.cancel()
-        super.onDestroy()
     }
 
 }
